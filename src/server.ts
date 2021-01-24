e4696cd7b903f60e982fa50d21cbddc2d00771c6
Jacques Lebourgeois <jacques@ochoeurdunet.org>
2021-01-24 21:33:26 +0100

    test appli angular

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import {RestApplication} from '@loopback/rest';
 import express from 'express';
 import {Server} from 'http';
 import pEvent from 'p-event';
+import path from 'path';
 import {ObjectTreesApplicationInterface} from './application';
 
 export class ExpressServer {
@@ -18,6 +19,21 @@ export class ExpressServer {
 
     // Serve static files in the public folder
     this.app.use(express.static('public'));
+    this.app.use(
+      '/admin',
+      express.static(
+        path.join(__dirname, '../node_modules/@jacquesparis/objects-angular'),
+      ),
+    );
+    this.app.use(
+      '/objectsites',
+      express.static(
+        path.join(
+          __dirname,
+          '../node_modules/@jacquesparis/objects-angular/objectsites',
+        ),
+      ),
+    );
   }
   async boot() {
     await this.lbApp.boot();
